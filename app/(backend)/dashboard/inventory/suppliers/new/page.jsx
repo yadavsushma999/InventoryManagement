cf353db38289187fab0d8f60e98554fceeb5d9d0
swaraj-tech <swarajtech999@gmail.com>
2025-06-09 13:39:18 +0530

    Supplier Page Bug Fixed

--- app/(backend)/dashboard/inventory/suppliers/new/page.jsx
+++ app/(backend)/dashboard/inventory/suppliers/new/page.jsx
@@ -4,6 +4,6 @@ import dynamic from 'next/dynamic';
 // app/(backend)/dashboard/inventory/brands/new/page.tsx
 const SupplierForm = dynamic(() => import('@/components/dashboard/SupplierForm'), { ssr: false });
 
-export default function SupplierForm() {
+export default function NewSupplier() {
   return <SupplierForm />
 }
