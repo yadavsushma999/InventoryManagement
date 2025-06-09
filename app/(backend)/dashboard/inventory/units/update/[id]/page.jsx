5cff2008159a58cf1e267d9dc19ef5fca8b636b6
swaraj-tech <swarajtech999@gmail.com>
2025-06-09 14:27:10 +0530

    Update Params Bug Fixed

--- app/(backend)/dashboard/inventory/units/update/[id]/page.jsx
+++ app/(backend)/dashboard/inventory/units/update/[id]/page.jsx
@@ -2,7 +2,8 @@
 import { getData } from '@/lib/getData';
 import UnitForm from '@/components/dashboard/UnitForm'
 
-export default async function Update({ params: { id } }) {
+export default async function Update({params }) {
+    const { id } = params;
     const data = await getData(`units/${id}`)
     return (
         <UnitForm initialData={data} isUpdate={true} />
--- app/(backend)/dashboard/inventory/warehouse/update/[id]/page.jsx
+++ app/(backend)/dashboard/inventory/warehouse/update/[id]/page.jsx
@@ -2,7 +2,8 @@
 import { getData } from '@/lib/getData';
 import WarehouseForm from '@/components/dashboard/WarehouseForm';
 
-export default async function Update({ params: { id } }) {
+export default async function Update({ params }) {
+    const { id } = params;
     const data = await getData(`warehouse/${id}`)
     return (
         <WarehouseForm initialData={data} isUpdate={true} />
