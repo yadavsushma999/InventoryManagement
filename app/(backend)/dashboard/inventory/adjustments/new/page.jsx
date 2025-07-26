8cfb63804a911315044c81b42e45e2b61a67ce29
swaraj-tech <swarajtech999@gmail.com>
2025-07-26 16:03:53 +0530

    Removed Console log

--- app/(backend)/dashboard/inventory/adjustments/new/page.jsx
+++ app/(backend)/dashboard/inventory/adjustments/new/page.jsx
@@ -1,16 +1,13 @@
 
 import AdjustmentForm from '@/components/dashboard/AdjustmentForm'
 import { getData } from '@/lib/getData'
-export const dynamic = "force-dynamic"
 
 export default async function NewAdjustments() {
   const itemsData = getData("items");
   const warehousesData = getData("warehouse");
   const supplierData = getData("suppliers");
- 
 
   const [items, warehouses, suppliers ]= await Promise.all([itemsData, warehousesData,supplierData]);
-   console.log("Items",items);
   return (
     <AdjustmentForm items={items} warehouses={warehouses} suppliers={suppliers} />
   );
